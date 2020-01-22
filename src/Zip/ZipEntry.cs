bee3b483076cfa0ae5a54e11dad765b4013158a3
Bastian Eicher <bastian@eicher.net>
2020-01-22 16:32:11 +0100

    Tweaked use of Unix timestamps

--- src/Zip/ZipEntry.cs
+++ src/Zip/ZipEntry.cs
@@ -1002,11 +1002,10 @@ namespace ICSharpCode.SharpZipLib.Zip
 			// Check for Unix timestamp
 			ExtendedUnixData unixData = extraData.GetData<ExtendedUnixData>();
 			if (unixData != null &&
-				// Only apply modification time, but require all other values to be present
+				// Only apply modification time, but require access time to be present
 				// This is done to match InfoZIP's behaviour
 				((unixData.Include & ExtendedUnixData.Flags.ModificationTime) != 0) &&
-				((unixData.Include & ExtendedUnixData.Flags.AccessTime) != 0) &&
-				((unixData.Include & ExtendedUnixData.Flags.CreateTime) != 0))
+				((unixData.Include & ExtendedUnixData.Flags.AccessTime) != 0))
 				return unixData.ModificationTime;
 
 			// Fall back to DOS time
